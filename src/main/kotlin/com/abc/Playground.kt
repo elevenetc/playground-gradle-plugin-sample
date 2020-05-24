856fcbb7d86a24ed4985f1f2c55b01c8c2e239f4
“Eugene <shumvlesu@gmail.com>
2020-05-24 21:22:02 +0200

    Add dumb plugin implementation

--- src/main/kotlin/com/abc/Playground.kt
+++ src/main/kotlin/com/abc/Playground.kt
@@ -7,5 +7,29 @@ class Playground : Plugin<Project> {
     override fun apply(project: Project) {
         val extension =
             project.extensions.create("playground", PlaygroundExtension::class.java, project.objects)
+
+
+        val buildTask = project.getTasksByName("build", true).first()
+
+        //val pgrTask = project.tasks.create("copyPlayground", PlaygroundTask::class.java, extension, buildTask)
+        //buildTask.dependsOn.add(pgrTask)
+
+        var start = 0L
+        var end = 0L
+
+        buildTask.doFirst {
+            start = System.currentTimeMillis()
+        }
+
+        buildTask.doLast {
+            end = System.currentTimeMillis()
+            println("build time:")
+            println("build time:")
+            println("build time:")
+            println("build time >>>> " + (end - start))
+            println("build time:")
+            println("build time:")
+            println("build time:")
+        }
     }
 }
--- src/main/kotlin/com/abc/PlaygroundTask.kt
+++ src/main/kotlin/com/abc/PlaygroundTask.kt
@@ -1,14 +1,15 @@
 package com.abc
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Task
 import org.gradle.api.tasks.TaskAction
 import javax.inject.Inject
 
-class PlaygroundTask @Inject constructor(
+open class PlaygroundTask @Inject constructor(
     private val extension: PlaygroundExtension
 ) : DefaultTask() {
     @TaskAction
     fun run() {
-        println("zed")
+
     }
 }
